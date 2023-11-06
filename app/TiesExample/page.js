6b90bd3188ab1c0156fdd8be6dfd66d90b11cc92
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-11-05 22:30:30 -0800

    Rename pg1 and pg2 to BeamsExample and TiesExample. Add Chords example. Rename project in package.json

---
+++ app/TiesExample/page.js
@@ -0,0 +1,110 @@
+"use client";
+import { Vex } from "vexflow";
+import { useEffect, useRef } from "react";
+
+export default function TiesExample() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const {
+      Renderer,
+      Stave,
+      StaveNote,
+      StaveTie,
+      Accidental,
+      Beam,
+      Formatter,
+      Dot,
+    } = Vex.Flow;
+
+    if (containerRef.current) {
+      const renderer = new Renderer(
+        containerRef.current,
+        Renderer.Backends.SVG
+      );
+
+      // Configure the rendering context.
+      renderer.resize(500, 500);
+      const rendererContext = renderer.getContext();
+      rendererContext.setFont("Arial", 10);
+
+      // Create a stave of width 400 at position 10, 40.
+      const stave = new Stave(10, 40, 400);
+
+      // Add a clef and time signature.
+      stave.addClef("treble").addTimeSignature("4/4");
+
+      // Connect it to the rendering context and draw!
+      stave.setContext(rendererContext).draw();
+
+      ////////////////////////
+
+      // Create the notes
+      const notes = [
+        dotted(
+          new StaveNote({
+            keys: ["e##/5"],
+            duration: "8d",
+          }).addModifier(new Accidental("##"))
+        ),
+        new StaveNote({
+          keys: ["b/4"],
+          duration: "16",
+        }).addModifier(new Accidental("b")),
+        new StaveNote({
+          keys: ["c/4"],
+          duration: "8",
+        }),
+        new StaveNote({
+          keys: ["d/4"],
+          duration: "16",
+        }),
+        new StaveNote({
+          keys: ["d/4"],
+          duration: "16",
+        }),
+        new StaveNote({
+          keys: ["d/4"],
+          duration: "q",
+        }),
+        new StaveNote({
+          keys: ["d/4"],
+          duration: "q",
+        }),
+      ];
+
+      const beams = Beam.generateBeams(notes);
+      Formatter.FormatAndDraw(rendererContext, stave, notes);
+      beams.forEach(function (b) {
+        b.setContext(rendererContext).draw();
+      });
+
+      const ties = [
+        new StaveTie({
+          first_note: notes[4],
+          last_note: notes[5],
+          first_indices: [0],
+          last_indices: [0],
+        }),
+        new StaveTie({
+          first_note: notes[5],
+          last_note: notes[6],
+          first_indices: [0],
+          last_indices: [0],
+        }),
+      ];
+
+      ties.forEach((t) => {
+        t.setContext(rendererContext).draw();
+      });
+
+      // A helper function to add a dot to a note.
+      function dotted(note) {
+        Dot.buildAndAttach([note]);
+        return note;
+      }
+    }
+  }, []);
+
+  return <div ref={containerRef} />;
+}
--- app/page.js
+++ app/page.js
@@ -6,8 +6,16 @@ export default function Home() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    const { Renderer, Stave, StaveNote, Accidental, Beam, Formatter, Dot } =
-      Vex.Flow;
+    const {
+      Renderer,
+      Stave,
+      StaveNote,
+      StaveTie,
+      Accidental,
+      Beam,
+      Formatter,
+      Dot,
+    } = Vex.Flow;
 
     if (containerRef.current) {
       const renderer = new Renderer(
@@ -16,57 +24,57 @@ export default function Home() {
       );
 
       // Configure the rendering context.
-      renderer.resize(500, 500);
+      renderer.resize(720, 130);
       const rendererContext = renderer.getContext();
       rendererContext.setFont("Arial", 10);
 
-      // Create a stave of width 400 at position 10, 40.
-      const stave = new Stave(10, 40, 400);
+      // Measure 1
+      const staveMeasure1 = new Stave(10, 0, 300);
+      staveMeasure1.addClef("treble").setContext(rendererContext).draw();
 
-      // Add a clef and time signature.
-      stave.addClef("treble").addTimeSignature("4/4");
-
-      // Connect it to the rendering context and draw!
-      stave.setContext(rendererContext).draw();
-
-      ////////////////////////
-
-      // Create the notes
-      const notes = [
-        dotted(
-          new StaveNote({ keys: ["e##/5"], duration: "8d" }).addModifier(
-            new Accidental("##")
-          )
-        ),
-        new StaveNote({ keys: ["b/4"], duration: "16" }).addModifier(
-          new Accidental("b")
-        ),
-        new StaveNote({ keys: ["c/4"], duration: "8" }),
-        new StaveNote({ keys: ["d/4"], duration: "16" }),
-        new StaveNote({ keys: ["e/4"], duration: "16" }).addModifier(
-          new Accidental("b")
-        ),
-        new StaveNote({ keys: ["d/4"], duration: "16" }),
-        new StaveNote({ keys: ["e/4"], duration: "16" }).addModifier(
-          new Accidental("#")
-        ),
-        new StaveNote({ keys: ["g/4"], duration: "32" }),
-        new StaveNote({ keys: ["a/4"], duration: "32" }),
-        new StaveNote({ keys: ["g/4"], duration: "16" }),
+      const notesMeasure1 = [
+        new StaveNote({ keys: ["c/4"], duration: "q" }),
         new StaveNote({ keys: ["d/4"], duration: "q" }),
+        new StaveNote({ keys: ["b/4"], duration: "qr" }),
+        new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
       ];
 
-      const beams = Beam.generateBeams(notes);
-      Formatter.FormatAndDraw(rendererContext, stave, notes);
-      beams.forEach((b) => {
-        b.setContext(rendererContext).draw();
-      });
+      // Helper function to justify and draw a 4/4 voice
+      Formatter.FormatAndDraw(rendererContext, staveMeasure1, notesMeasure1);
 
-      // A helper function to add a dot to a note.
-      function dotted(note) {
-        Dot.buildAndAttach([note]);
-        return note;
-      }
+      // Measure 2 - second measure is placed adjacent to first measure.
+      const staveMeasure2 = new Stave(
+        staveMeasure1.width + staveMeasure1.x,
+        0,
+        400
+      );
+
+      const notesMeasure2_part1 = [
+        new StaveNote({ keys: ["c/4"], duration: "8" }),
+        new StaveNote({ keys: ["d/4"], duration: "8" }),
+        new StaveNote({ keys: ["b/4"], duration: "8" }),
+        new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "8" }),
+      ];
+
+      const notesMeasure2_part2 = [
+        new StaveNote({ keys: ["c/4"], duration: "8" }),
+        new StaveNote({ keys: ["d/4"], duration: "8" }),
+        new StaveNote({ keys: ["b/4"], duration: "8" }),
+        new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "8" }),
+      ];
+
+      // Create the beams for 8th notes in second measure.
+      const beam1 = new Beam(notesMeasure2_part1);
+      const beam2 = new Beam(notesMeasure2_part2);
+
+      const notesMeasure2 = notesMeasure2_part1.concat(notesMeasure2_part2);
+
+      staveMeasure2.setContext(rendererContext).draw();
+      Formatter.FormatAndDraw(rendererContext, staveMeasure2, notesMeasure2);
+
+      // Render beams
+      beam1.setContext(rendererContext).draw();
+      beam2.setContext(rendererContext).draw();
     }
   }, []);
 
